b02b1893eabe66e16f87c9b211b31d0035ba3060
jkhoang313 <jkhoang313@gmail.com>
2016-11-30 14:20:21 -0500

    passed tests

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(".")
+    array.each_with_index {|element, index| array[index] = element.split("?") if element.include?("?")}
+    array = array.flatten
+    array.each_with_index {|element, index| array[index] = element.split("!") if element.include?("!")}
+    array.flatten.delete_if {|element| element == ""}.count
   end
 end
